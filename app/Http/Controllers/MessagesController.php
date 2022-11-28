ac1192ad796fda4cc853c86ed5d5f66caa7a8f4f
Daymer Alberto Alviz Alvarez <daymer21@gmail.com>
2022-11-28 16:51:43 -0500

    send email in log, screen
    send email in log, screen

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
 
 class MessagesController extends Controller
 {
     public function store()
     {
-         request()->validate([
+        $message =  request()->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject'=> 'required',
@@ -18,6 +20,12 @@ class MessagesController extends Controller
            'name.required' => __('I need you name'), 
         ]);
 
-         return 'Datos validados';
+        // enviar el email
+
+        Mail::to('daymer21@gmail.com')->queue(new MessageReceived($message));
+
+       // return new MessageReceived($message);
+
+         return 'Mensaje enviado';
     }
 }
